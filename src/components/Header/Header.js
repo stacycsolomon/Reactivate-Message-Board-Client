7347e400a0bec582afc436b1c1818280f230a4d1
BrittGitHub <brittany.cavendish@gmail.com>
2022-04-29 14:13:55 -0400

    updated project name to match github project name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -26,7 +26,7 @@ const alwaysOptions = (
 const Header = ({ user }) => (
   <Navbar bg='primary' variant='dark' expand='md'>
     <Navbar.Brand>
-      <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}>Reactivate-Message-Board</Link>
+      <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}>Reactivate-Message-Board-Client</Link>
     </Navbar.Brand>
     <Navbar.Toggle aria-controls='basic-navbar-nav' />
     <Navbar.Collapse id='basic-navbar-nav'>
