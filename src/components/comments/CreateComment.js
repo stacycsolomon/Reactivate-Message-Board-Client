d99b9b5556038c4f6ac2796728fabb656abad7cc
Stacy S <stacycsolomon@gmail.com>
2022-05-05 15:11:43 -0500

    Worked on update comment feature
    Co-authored by: Brittany Cavendish <brittany.cavendish@gmail.com>

--- src/components/comments/CreateComment.js
+++ src/components/comments/CreateComment.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-mixed-spaces-and-tabs */
-/* eslint-disable no-tabs */
 import React, { Component } from 'react'
 import { createComment } from '../../api/comment'
 import Button from 'react-bootstrap/Button'
@@ -17,62 +15,61 @@ class CreateComment extends Component {
     }
   }
 
-	handleChange = (event) =>
-	  this.setState({
-	    [event.target.name]: event.target.value
-	  })
+  handleChange = (event) =>
+    this.setState({
+      [event.target.name]: event.target.value
+    })
 
-	handleSubmit = (event) => {
-	  event.preventDefault()
+  handleSubmit = (event) => {
+    event.preventDefault()
 
-	  const { user, msgAlert, history, match } = this.props
+    const { user, msgAlert, history, match } = this.props
 
-	  // createComment(this.state, match.params.id, user)
-	  createComment(this.state, user)
-	    .then(() => history.push('/posts/' + match.params.id))
-	    .then(() => {
-	      msgAlert({
-	        heading: 'Comment created successfully',
-	        message: 'Comment created!',
-	        variant: 'success'
-	      })
-	    })
-	    .catch((error) => {
-	      msgAlert({
-	        heading: 'Failed to create a comment on post',
-	        message: 'Create comment error: ' + error.message,
-	        variant: 'danger'
-	      })
-	    })
-	}
+    createComment(this.state, user)
+      .then(() => history.push('/posts/' + match.params.id))
+      .then(() => {
+        msgAlert({
+          heading: 'Comment created successfully',
+          message: 'Comment created!',
+          variant: 'success'
+        })
+      })
+      .catch((error) => {
+        msgAlert({
+          heading: 'Failed to create a comment on post',
+          message: 'Create comment error: ' + error.message,
+          variant: 'danger'
+        })
+      })
+  }
 
-	render () {
-	  return (
-	    <Form onSubmit={this.handleSubmit}>
-	      <Form.Group controlId='title'>
-	        <Form.Label>Comment Title</Form.Label>
-	        <Form.Control
-	          required
-	          name='title'
-	          value={this.state.title}
-	          placeholder='Post title'
-	          onChange={this.handleChange}
-	        />
-	      </Form.Group>
-	      <Form.Group controlId='content'>
-	        <Form.Label>Summary</Form.Label>
-	        <Form.Control
-	          required
-	          name='content'
-	          value={this.state.content}
-	          placeholder='Add comment here'
-	          onChange={this.handleChange}
-	        />
-	      </Form.Group>
-	      <Button type='submit'>Submit</Button>
-	    </Form>
-	  )
-	}
+  render () {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Group controlId='title'>
+          <Form.Label>Comment Title</Form.Label>
+          <Form.Control
+            required
+            name='title'
+            value={this.state.title}
+            placeholder='Comment title'
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Form.Group controlId='content'>
+          <Form.Label>Summary</Form.Label>
+          <Form.Control
+            required
+            name='content'
+            value={this.state.content}
+            placeholder='Add comment here'
+            onChange={this.handleChange}
+          />
+        </Form.Group>
+        <Button type='submit'>Submit</Button>
+      </Form>
+    )
+  }
 }
 
 export default withRouter(CreateComment)
