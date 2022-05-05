84c295192b9b2302cc4cda145c9ba233c471744f
Stephanie <stephanie@Stephanies-MacBook-Air-3.local>
2022-05-05 12:00:48 -0400

    added some design

--- src/components/posts/ShowPost.js
+++ src/components/posts/ShowPost.js
@@ -64,43 +64,72 @@ class ShowPost extends Component {
       const { title, text, date, comments, owner } = this.state.post
       console.log(comments)
       const { user, history, match } = this.props
-
+      const commentStyles = {
+        border: '3px solid black',
+        margin: '5px auto',
+        'border-radius': '10px',
+        'border-color': 'grey',
+        'text-align': 'left',
+        width: '80%',
+        'align-self': 'center',
+        'padding-left': '10px'
+      }
+      const postStyles = {
+        border: '2px solid black',
+        // margin: 'auto 2px',
+        'margin-top': '15px',
+        'border-radius': '10px',
+        'border-color': 'grey',
+        'text-align': 'center',
+        'align-content': 'center',
+        padding: '10px',
+        'font-family': 'Times New Roman, Times, serif'
+      }
+      const commentSpanStyle = {
+        'font-size': '10px'
+      }
+      const titleSpanStyle = {
+        'font-size': '15px'
+      }
+      const buttonStyle = {
+        margin: '4px'
+      }
       const commentsJSX = []
       for (let i = 0; i < comments.length; i++) {
         console.log(comments[i])
         commentsJSX.push(
-          <>
-            <p>{comments[i].title} by {comments[i]?.username}</p>
+          <div style={commentStyles}>
+            <h6>{comments[i].title}  <span style={commentSpanStyle}>by: {comments[i]?.username}</span></h6>
             <p>{comments[i].content}</p>
-          </>
+          </div>
         )
       }
       console.log(this.state.post.owner)
 
       return (
-        <>
-          <h3>Show a Post</h3>
+        <div style={postStyles}>
+          {/* <h3>Show a Post</h3> */}
 
-          <h4>{title} by {owner.username}</h4>
-          <h5>{text}</h5>
+          <h4><span style={{ 'text-decoration': 'underline' }}>{title}</span> <br/>  <span style={titleSpanStyle}>by: {owner.username}</span></h4>
+          <p>{text}</p>
           <h6>{date.slice(0, 10)}</h6>
-          <h3>Comments:</h3>
-          {commentsJSX}
 
           {user._id === owner._id && (
             <>
-              <Button onClick={this.handleDelete}>Delete</Button>
-              <Button
+              <Button style={buttonStyle} onClick={this.handleDelete}>Delete</Button>
+              <Button style={buttonStyle}
                 onClick={() => history.push(`/posts/${match.params.id}/edit`)}>
 								Update
               </Button>
             </>
           )}
-          <Button
+          <Button style={buttonStyle}
             onClick={() => history.push(`/posts/${match.params.id}/create-comment`)}>
 						Add Comment
           </Button>
-        </>
+          <h3>Comments:</h3>
+          {commentsJSX}
+        </div>
       )
     }
 }
